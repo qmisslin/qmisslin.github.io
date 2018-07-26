c05b0be4d923dc866d80ccfbe158b2c5975f0b90
Misslin Quentin <misslin.quentin@gmail.com>
2018-07-27 01:18:45 +0200

    feat(site-cercle-europeen): Header style update

--- CercleEuropéen/script.js
+++ CercleEuropéen/script.js
@@ -1,19 +1,28 @@
 
+addStyle = (_dom, _styleObj) => {
+    if(_dom.style == null) _dom.style = "";
+    Object.keys(_styleObj).forEach(key => _dom.style[key] = _styleObj[key]);
+};
+selAll = (_sel) => Array.from(document.querySelectorAll(_sel));
+sel = (_sel) => document.querySelector(_sel);
 
-
+function refresh_background(){
+    selAll('.scroll-background').forEach(
+        domElement => {
+            _top = domElement.getBoundingClientRect().top;
+            addStyle(domElement, {'background-position': '50% '+(-_top)+'px'}); //
+            console.log(_top);
+        });
+}
 
 $(document).ready(function() {
 
-	addStyle = (_dom, _styleObj) => {
-		if(_dom.style == null) _dom.style = {};
-		Object.assign(_dom.style, _styleObj);
-	};
+	
 
-	classSelector = (_class) => Array.from(document.getElementsByClassName(_class));
-	idSelector = (_id) => document.getElementById(_id);
+	
 
     duplicateModels = function(){
-    	classSelector('model')
+    	selAll('.model')
     		.forEach(_dom => {
     			var clone = _dom.cloneNode();
     			clone.classList.add('duplicate');
@@ -22,13 +31,13 @@ $(document).ready(function() {
     };
 
     initStarPath = function(){
-    	classSelector('star').forEach((_dom, _index) => {
+    	selAll('.star').forEach((_dom, _index) => {
     		_dom.setAttribute('d', 'M301.113,12.011l99.25,179.996l201.864,38.778L461.706,380.808 ' +
 			'l25.508,203.958l-186.101-87.287L115.01,584.766l25.507-203.958L0,' +
 			'230.785l201.86-38.778L301.113,12.011');
 			_dom.id = "star-" + _index;
 			addStyle(_dom, {
-				'transform': 'translateX(-50%) rotate('+(_index/12*360)+'deg) translateX(3300px)'
+				'transform': 'translateX(-50%) rotate('+(_index/12*360)+'deg) translateX(600px) scale(0.3) '
 			});
 		});
     }
@@ -37,7 +46,10 @@ $(document).ready(function() {
     	duplicateModels();
     	initStarPath();
 
-    	idSelector('body').classList.remove('init-animate-state');
-    }
+    	sel('#body').classList.remove('init-animate-state');
+
+        console.log("Set attribute");
+        sel('#body').setAttribute("onscroll", "refresh_background()");
+    }   
     init();
 });
